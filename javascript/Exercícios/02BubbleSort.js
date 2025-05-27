65cb3ee5f8eec17e813e7c57126ac446629d7464
matheus da rocha sena <matheus.sena@dataprev.gov.br>
2025-05-27 15:08:49 -0300

    adicionando algoritmo de bubble sort

--- javascript/Exercícios/02BubbleSort.js
+++ javascript/Exercícios/02BubbleSort.js
@@ -1,31 +1,46 @@
-BubbleSort = (vetor) => {
+// BubbleSort = (vetor) => {
 
-for (i = 0; i < vetor.length; i++){
-    for (j = 0; j < vetor.length; j++)
-        if (vetor[i] > vetor[j + 1]){
-            let prov = vetor[i];
-            vetor[i] = vetor[j + 1];
-            vetor[j + 1] = prov;
-            
-            console.log(`Troquei ${vetor[i]} na posição ${i} por ${vetor[j+1]} na posicção ${j + 1}`)
-        }
-    }   
+// for (i = 0; i < vetor.length; i++){
+//     for (j = 0; j < vetor.length; j++)
+//         if (vetor[i] > vetor[j + 1])
+//             {
+//             let prov = vetor[i];
+//             vetor[i] = vetor[j + 1];
+//             vetor[j + 1] = prov;
+//             console.log(`Troquei ${vetor[i]} na posição ${i} por ${vetor[j+1]} na posicção ${j + 1}`);
+//             }
+//     }   
+// }
+const bubbleSort = (vetor) => {
+    let i = 0;
+    while (i < vetor.length) {
+
+        let j = 0;
+        while(j < vetor.length){
+
+            if(vetor[j] > vetor[j + 1]){
+
+                let prov = vetor[j];
+                vetor[j] = vetor[j + 1];
+                vetor[j + 1] = prov;
+            }
+        j++;   
+    }
+    i++;
+    }
 }
 
 
 
+const vetor = [4, 5, 2, 1];
+const numeros = [23, 5, 12, 88, 3, 34, 90, 17, 8, 45, 76, 29, 1, 99, 21, 65, 38, 7, 19, 54, 11, 6, 40, 84, 31, 70, 92, 25, 36, 15, 47, 73, 13, 2, 68, 59, 80, 9, 27, 58];
 
-
-// [  ,  ,  ,  ,  ,  ,  ,  ]
-//0  1  2  3  4  5  6  7
-const vetor = [4, 3, 2, 1];
-//       0  1  2  3  4  5  6
 
 console.log(vetor);
 
-BubbleSort(vetor);
+bubbleSort(numeros);
 
-console.log(vetor);
+console.log(numeros);
 
 
 
---
+++ javascript/Exercícios/test.js
@@ -0,0 +1,9 @@
+let j = 0;
+while(j < 4){
+
+console.log(j)        
+
+j++;
+
+
+}
