012935df8aade259f1d94bb6d537893b454fbaa6
matheus da rocha sena <matheus.sena@dataprev.gov.br>
2025-09-02 17:27:36 -0300

    adding express and NodeJS files

--- API/server.js
+++ API/server.js
@@ -1,5 +1,9 @@
+//This line imports express into your project.
 const express = require('express');
+
+//This line will create an Express instance and its reference will be stored by app
 const app = express();
+
 
 app.get('/', (req, res) => {
   res.send('Olá, mundo!');
